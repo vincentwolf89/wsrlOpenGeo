4eadc019831a90c9eec0744579161e001425bbef
vincent van der wolf <vincent.vanderwolf@gmail.com>
2022-12-04 11:15:50 +0100

    fix dike segments

--- locateDikeSegments.py
+++ locateDikeSegments.py
@@ -18,8 +18,15 @@ till_field = "dp_tot"
 ## create routes for dp :
 # split trajectory on dp
 arcpy.management.SplitLineAtPoint(dike_trajectory, dike_refpoints, "temp_trajectory_split", "{} Meters".format(route_tolerance))
+
 # create segment routes
-arcpy.lr.CreateRoutes("temp_trajectory_split", "ORIG_SEQ", "temp_routes_trajectory", "LENGTH", None, None, "UPPER_LEFT", 1, 0, "IGNORE", "INDEX")
+arcpy.AddField_management("temp_trajectory_split", "from_field", "DOUBLE", 2, field_is_nullable="NULLABLE")
+arcpy.AddField_management("temp_trajectory_split", "till_field", "DOUBLE", 2, field_is_nullable="NULLABLE")
+
+arcpy.management.CalculateField("temp_trajectory_split", "from_field", "0", "PYTHON3", '', "TEXT", "NO_ENFORCE_DOMAINS")
+arcpy.management.CalculateField("temp_trajectory_split", "till_field", "!Shape_Length!", "PYTHON3", '', "TEXT", "NO_ENFORCE_DOMAINS")
+
+arcpy.lr.CreateRoutes("temp_trajectory_split", "ORIG_SEQ", "temp_routes_trajectory", "TWO_FIELDS", "from_field", "till_field", "", "1","0", "IGNORE", "INDEX")
 
 # generate startpoints for splits
 arcpy.management.FeatureVerticesToPoints("temp_trajectory_split", "temp_startpoints", "START")
@@ -46,27 +53,30 @@ arcpy.management.FeatureVerticesToPoints(dike_segments, "temp_endpoints_segments
 arcpy.lr.LocateFeaturesAlongRoutes("temp_startpoints_segments", "temp_routes_trajectory", "ORIG_SEQ", "{} Meters".format(route_tolerance), "temp_located_startpoints", "RID; Point; MEAS", "FIRST", "DISTANCE", "ZERO", "FIELDS", "M_DIRECTON")
 arcpy.lr.LocateFeaturesAlongRoutes("temp_endpoints_segments", "temp_routes_trajectory", "ORIG_SEQ", "{} Meters".format(route_tolerance), "temp_located_endpoints", "RID; Point; MEAS", "FIRST", "DISTANCE", "ZERO", "FIELDS", "M_DIRECTON")
 
-arcpy.management.JoinField("temp_located_startpoints", "RID", "temp_routes_trajectory", "ORIG_SEQ", "start_id", "NOT_USE_FM", None)
-arcpy.management.JoinField("temp_located_endpoints", "RID", "temp_routes_trajectory", "ORIG_SEQ", "end_id", "NOT_USE_FM", None)
 
-# join meas from locate
-arcpy.management.JoinField("temp_startpoints_segments", id_field, "temp_located_startpoints", "ORIG_FID", "MEAS", "NOT_USE_FM", None)
-arcpy.management.JoinField("temp_endpoints_segments", id_field, "temp_located_endpoints", "ORIG_FID", "MEAS", "NOT_USE_FM", None)
+# join meas and RID from locate
+arcpy.management.JoinField("temp_startpoints_segments", id_field, "temp_located_startpoints", id_field, "RID;MEAS", "NOT_USE_FM", None)
+arcpy.management.JoinField("temp_endpoints_segments", id_field, "temp_located_endpoints", id_field, "RID;MEAS", "NOT_USE_FM", None)
+
+
+arcpy.management.JoinField("temp_startpoints_segments", "RID", "temp_routes_trajectory", "ORIG_SEQ", "start_id", "NOT_USE_FM", None)
+arcpy.management.JoinField("temp_endpoints_segments", "RID", "temp_routes_trajectory", "ORIG_SEQ", "start_id", "NOT_USE_FM", None)
+
+# do some renaming
+arcpy.management.AlterField("temp_startpoints_segments", "MEAS", "start_distance", '', "DOUBLE", 8, "NULLABLE", "CLEAR_ALIAS")
+arcpy.management.AlterField("temp_endpoints_segments", "MEAS", "end_distance", '', "DOUBLE", 8, "NULLABLE", "CLEAR_ALIAS")
+arcpy.management.AlterField("temp_endpoints_segments", "start_id", "end_id", '', "TEXT", 24, "NULLABLE", "CLEAR_ALIAS")
 
 # join meas fields to dike segments
 arcpy.management.CopyFeatures(dike_segments, "temp_ouput_segments")
 
-arcpy.management.JoinField("temp_ouput_segments", id_field, "temp_located_startpoints", id_field, "start_id", "NOT_USE_FM", None)
-arcpy.management.JoinField("temp_ouput_segments", id_field, "temp_located_endpoints", id_field, "end_id", "NOT_USE_FM", None)
+arcpy.management.JoinField("temp_ouput_segments", id_field, "temp_startpoints_segments", id_field, "start_id;start_distance", "NOT_USE_FM", None)
+arcpy.management.JoinField("temp_ouput_segments", id_field, "temp_endpoints_segments", id_field, "end_id;end_distance", "NOT_USE_FM", None)
 
-arcpy.management.AlterField("temp_ouput_segments", "MEAS", "start_distance", '', "DOUBLE", 8, "NULLABLE", "DO_NOT_CLEAR")
-arcpy.management.AlterField("temp_ouput_segments", "MEAS_1", "end_distance", '', "DOUBLE", 8, "NULLABLE", "DO_NOT_CLEAR")
-
-# add new fields with correct name
+# # add new fields with correct name
 arcpy.AddField_management("temp_ouput_segments", from_field, "TEXT", 2)
 arcpy.AddField_management("temp_ouput_segments", till_field, "TEXT", 2)
 
 arcpy.management.CalculateField("temp_ouput_segments", from_field, "$feature.start_id + '+'+ Text(Round($feature.start_distance,0))", "ARCADE")
 arcpy.management.CalculateField("temp_ouput_segments", till_field, "$feature.end_id + '+'+ Text(Round($feature.end_distance,0))", "ARCADE")
-
 
