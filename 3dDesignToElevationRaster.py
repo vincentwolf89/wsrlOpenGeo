d8ecf1e3dd478e11e74c447e6c92da462799b4fb
Vincent van der Wolf <51679701+vincentwolf89@users.noreply.github.com>
2023-11-16 19:31:14 +0100

    updates

--- 3dDesignToElevationRaster.py
+++ 3dDesignToElevationRaster.py
@@ -1,19 +1,23 @@
 import arcpy
+arcpy.env.overwriteOutput = True
+arcpy.env.workspace = r"C:\Users\vince\Mijn Drive\WSRL\safe_data\safe_data\safe_data.gdb"
 
-
-
+temp_tin = r"C:\Users\vince\Mijn Drive\WSRL\safe_data\safe_data\tin"
+in_features = "_3D_KA1_ruimtebeslag"
+cash_location = r"C:\Users\vince\Mijn Drive\WSRL\safe_data\safe_data\cash"
 
 
 arcpy.ddd.CreateTin(
-    out_tin=r"C:\Users\vince\Mijn Drive\WSRL\safe_data\safe_data\tin_test",
+    out_tin=temp_tin,
     spatial_reference='PROJCS["RD_New",GEOGCS["GCS_Amersfoort",DATUM["D_Amersfoort",SPHEROID["Bessel_1841",6377397.155,299.1528128]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Double_Stereographic"],PARAMETER["False_Easting",155000.0],PARAMETER["False_Northing",463000.0],PARAMETER["Central_Meridian",5.38763888888889],PARAMETER["Scale_Factor",0.9999079],PARAMETER["Latitude_Of_Origin",52.15616055555555],UNIT["Meter",1.0]]',
-    in_features="_3D_KA1_taludarcering Shape.Z Hard_Line <None>",
+    in_features="{} SHAPE.Z Soft_Clip OBJECTID".format(in_features),
     constrained_delaunay="DELAUNAY"
 )
 
+
 arcpy.ddd.TinRaster(
-    in_tin="tin_test__",
-    out_raster=r"c:\users\vince\mijn drive\wsrl\safe_data\safe_data\safe_data.gdb\tin_te_tinra",
+    in_tin=temp_tin,
+    out_raster="temp_tin_raster",
     data_type="FLOAT",
     method="LINEAR",
     sample_distance="CELLSIZE",
@@ -22,19 +26,19 @@ arcpy.ddd.TinRaster(
 )
 
 arcpy.management.Clip(
-    in_raster="tin_te_tinra",
-    rectangle="112821.736600004 436351.827700004 140075.428400002 445340.897200003",
-    out_raster=r"C:\Users\vince\Mijn Drive\WSRL\safe_data\safe_data\safe_data.gdb\tin_te_tinra_Clip",
-    in_template_dataset="_3D_KA1_ruimtebeslag",
+    in_raster="temp_tin_raster",
+    rectangle="#",
+    out_raster="temp_tin_raster_clip",
+    in_template_dataset=in_features,
     nodata_value="3,4e+38",
     clipping_geometry="ClippingGeometry",
     maintain_clipping_extent="MAINTAIN_EXTENT"
 )
 
 arcpy.management.ProjectRaster(
-    in_raster="tin_te_tinra_Clip",
-    out_raster=r"C:\Users\vince\Mijn Drive\WSRL\safe_data\safe_data\safe_data.gdb\tin_te_tinra_C_ProjectRaster",
-    out_coor_system='GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]]',
+    in_raster="temp_tin_raster_clip",
+    out_raster="temp_tin_raster_clip_projected",
+    out_coor_system='PROJCS["WGS_1984_Web_Mercator_Auxiliary_Sphere",GEOGCS["GCS_WGS_1984",DATUM["D_WGS_1984",SPHEROID["WGS_1984",6378137.0,298.257223563]],PRIMEM["Greenwich",0.0],UNIT["Degree",0.0174532925199433]],PROJECTION["Mercator_Auxiliary_Sphere"],PARAMETER["False_Easting",0.0],PARAMETER["False_Northing",0.0],PARAMETER["Central_Meridian",0.0],PARAMETER["Standard_Parallel_1",0.0],PARAMETER["Auxiliary_Sphere_Type",0.0],UNIT["Meter",1.0]]',
     resampling_type="NEAREST",
     cell_size="0,499994345784074 0,499976055397925",
     geographic_transform="Amersfoort_To_WGS_1984_NTv2",
@@ -46,26 +50,27 @@ arcpy.management.ProjectRaster(
 
 
 arcpy.management.ManageTileCache(
-    in_cache_location=r"C:\Users\vince\Mijn Drive\WSRL\safe_data\safe_data\cash",
+    in_cache_location= cash_location,
     manage_mode="RECREATE_ALL_TILES",
-    in_cache_name="test",
-    in_datasource="tin_te_tinra_Clip",
+    in_cache_name="cash_{}".format(in_features),
+    in_datasource="temp_tin_raster_clip_projected",
     tiling_scheme="ARCGISONLINE_ELEVATION_SCHEME",
     import_tiling_scheme=None,
     scales=[577790.554289,288895.277144,144447.638572,72223.819286,36111.909643,18055.954822,9027.977411,4513.988705,2256.994353],
-    area_of_interest=r"in_memory\feature_set1",
+    # area_of_interest=r"in_memory\feature_set1",
     max_cell_size=None,
     min_cached_scale=577790.554289,
     max_cached_scale=2256.994353
 )
 
 arcpy.management.ExportTileCache(
-    in_cache_source="test",
-    in_target_cache_folder=r"C:\Users\vince\Mijn Drive\WSRL\safe_data\safe_data\cash",
-    in_target_cache_name="test_cash",
+    in_cache_source=cash_location+"\cash_{}".format(in_features),
+    in_target_cache_folder=cash_location,
+    in_target_cache_name="cash_pack_{}".format(in_features),
     export_cache_type="TILE_PACKAGE_TPKX",
     storage_format_type="COMPACT",
     scales=[1155581.108577,577790.554289,288895.277144,144447.638572,72223.819286,36111.909643,18055.954822,9027.977411,4513.988705],
-    area_of_interest=r"in_memory\feature_set1"
+    # area_of_interest=r"in_memory\feature_set1"
 )
+
 
