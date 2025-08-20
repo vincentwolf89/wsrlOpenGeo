19fb8e749c672b96fbbb5586c6ed4dedaf7dcc80
vincentwolf89 <vincent.vanderwolf@gmail.com>
2025-08-20 15:16:31 +0200

    test

--- vertigis-wsrl/src/components/ElevationExaggarator/ElevationExaggeratorModel.ts
+++ vertigis-wsrl/src/components/ElevationExaggarator/ElevationExaggeratorModel.ts
@@ -65,7 +65,7 @@ export default class ElevationExaggeratorModel extends ComponentModelBase<Elevat
 
             
 
-    
+            // https://developers.arcgis.com/javascript/latest/sample-code/layers-custom-elevation-exaggerated/
        
          
 
